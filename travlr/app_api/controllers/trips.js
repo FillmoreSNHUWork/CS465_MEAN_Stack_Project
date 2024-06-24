465a4c46ef347505413f0a195c82e67d57e2055e
coltrain80 <coltrain8180@gmail.com>
2024-06-24 00:59:29 -0500

    module 7 nearly complete...issues with JWT

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -47,72 +47,92 @@ const tripsFindByCode = async(req, res) => {
 }
 
 const tripsAddTrip = async (req, res) => {
-    const newTrip = new Trip({
-        code: req.body.code,
-        name: req.body.name,
-        length: req.body.length,
-        start: req.body.start,
-        resort: req.body.resort,
-        perPerson: req.body.perPerson,
-        image: req.body.image,
-        description: req.body.description
+    getUser(req, res, (req, res) => {
+      Trip.create(
+        {
+          code: req.body.code,
+          name: req.body.name,
+          length: req.body.length,
+          start: req.body.start,
+          resort: req.body.resort,
+          perPerson: req.body.perPerson,
+          image: req.body.image,
+          description: req.body.description
+        },
+        (err, trip) => {
+          if (err) {
+            return res.status(400).json(err); // Bad request
+          } else {
+            return res.status(201).json(trip); // Created
+          }
+        }
+      );
     });
-
-    try {
-        const q = await newTrip.save();
-        return res.status(201).json(q);
-    } catch (err) {
-        return res.status(400).json(err);
-    }
-}
+  };
+  
 
 // PUT: /trips/:tripCode - Updates a Trip
 // PUT: /trips/:tripCode - Updates an existing Trip
 // Regardless of outcome, response must include HTML status code and JSON message to the requesting client
-const tripsUpdateTrip = async(req, res) => {
-    // Uncomment for debugging
-    console.log(req.params);
-    console.log(req.body);
-
-    try {
-        const q = await Model.findOneAndUpdate(
-            { 'code' : req.params.tripCode },
-            {
-                code: req.body.code,
-                name: req.body.name,
-                length: req.body.length,
-                start: req.body.start,
-                resort: req.body.resort,
-                perPerson: req.body.perPerson,
-                image: req.body.image,
-                description: req.body.description
-            },
-            { new: true, runValidators: true } // Options to return the new document and run validations
-        ).exec();
-
-        if(!q) {
-            // Database returned no data
-            return res
-                .status(400)
-                .json({ message: 'Trip not found or not updated' });
-        } else {
-            // Return resulting updated trip
-            return res
-                .status(201)
-                .json(q);
+const tripsUpdateTrip = async (req, res) => {
+    getUser(req, res, (req, res) => {
+      Trip.findOneAndUpdate(
+        { 'code': req.params.tripCode },
+        {
+          code: req.body.code,
+          name: req.body.name,
+          length: req.body.length,
+          start: req.body.start,
+          resort: req.body.resort,
+          perPerson: req.body.perPerson,
+          image: req.body.image,
+          description: req.body.description
+        },
+        { new: true }
+      )
+      .then(trip => {
+        if (!trip) {
+          return res.status(404).send({
+            message: "Trip not found with code " + req.params.tripCode
+          });
         }
-    } catch (err) {
-        return res
-            .status(500)
-            .json(err);
+        res.send(trip);
+      })
+      .catch(err => {
+        if (err.kind === 'ObjectId') {
+          return res.status(404).send({
+            message: "Trip not found with code " + req.params.tripCode
+          });
+        }
+        return res.status(500).json(err); // Server error
+      });
+    });
+  };
+  
+
+const getUser = (req, res, callback) => {
+    const userName = req.body.userName;  // Assuming userName is obtained from the request body
+    const locationId = req.params.locationid;
+  
+    if (locationId) {
+      Loc.findById(locationId)
+        .select('reviews')
+        .exec((err, location) => {
+          if (err) {
+            return res.status(400).json(err);
+          } else {
+            doAddReview(req, res, location, userName);  // Call the function with userName
+          }
+        });
+    } else {
+      res.status(404).json({ "message": "Location not found" });
     }
-    // Uncomment the following line to show results of operation on the console
-    // console.log(q);
-};
+  };  
 
 module.exports = {
     tripsList,
     tripsFindByCode,
     tripsAddTrip,
-    tripsUpdateTrip
+    tripsUpdateTrip,
+    getUser
 };
--- travlr/app_api/routes/index.js
+++ travlr/app_api/routes/index.js
@@ -1,19 +1,38 @@
 const express = require('express'); // Express app
 const router = express.Router(); // Router logic
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
 
 // This is where we import the controllers we intend to route
 const tripsController = require('../controllers/trips');
+
+// Auth controller
+const authController = require('../controllers/authentication');
+
+// User
+//const registerController = require('../controllers/')
 
 // define route for our trips endpoint
 router
     .route('/trips')
     .get(tripsController.tripsList) // GET Method routes triplist
-    .post(tripsController.tripsAddTrip); // POST Method Adds a Trip
+    .post(auth, tripsController.tripsAddTrip) // POST Method Adds a Trip
+
+router
+    .route('/login')
+    .post(authController.login); // POST Method to login
+
+router
+    .route('/register')
+    .post(authController.register);
 
 // GET Method routes tripsFindByCode - requires parameter
 router
     .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode)
-    .put(tripsController.tripsUpdateTrip);
+    .put(auth, tripsController.tripsUpdateTrip);
 
 module.exports = router;
