11361d66a4dd283aa35a15bc477847cf6d2bba2a
coltrain80 <coltrain8180@gmail.com>
2024-06-23 23:14:11 -0500

    work for app_admin and security adds

---
+++ app_admin/src/app/edit-trip/edit-trip.component.ts
@@ -0,0 +1,96 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from "@angular/forms";
+import { TripDataService } from '../services/trip-data.service';
+import { Trip } from '../models/trips';
+
+@Component({
+  selector: 'app-edit-trip',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './edit-trip.component.html',
+  styleUrl: './edit-trip.component.css'
+})
+export class EditTripComponent implements OnInit {
+  public editForm!: FormGroup;
+  trip!: Trip;
+  submitted = false;
+  message: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private tripDataService: TripDataService
+  ) {}
+
+  ngOnInit(): void {
+    // Retrieve stashed trip ID
+    let tripCode = localStorage.getItem("tripCode");
+    if (!tripCode) {
+      alert("Something wrong, couldn't find where I stashed tripCode!");
+      this.router.navigate(['']);
+      return;
+    }
+    console.log('EditTripComponent::ngOnInit');
+    console.log('tripcode:' + tripCode);
+
+    this.editForm = this.formBuilder.group({
+      _id: [],
+      code: [tripCode, Validators.required],
+      name: ['', Validators.required],
+      length: ['', Validators.required],
+      start: ['', Validators.required],
+      resort: ['', Validators.required],
+      perPerson: ['', Validators.required],
+      image: ['', Validators.required],
+      description: ['', Validators.required]
+    });
+
+    this.tripDataService.getTrip(tripCode)
+      .subscribe({
+        next: (value: any) => {
+          this.trip = value;
+          // Convert start date to a format suitable for the input field
+          value[0].start = new Date(value[0].start).toISOString().substring(0, 10);
+          // Populate our record into the form
+          this.editForm.patchValue(value[0]);
+          if (!value) {
+            this.message = 'No Trip Retrieved!';
+          } else {
+            this.message = 'Trip: ' + tripCode + ' retrieved';
+          }
+          console.log(this.message);
+        },
+        error: (error: any) => {
+          console.log('Error: ' + error);
+        }
+      });
+  }
+
+  // Method for form submission handling
+  public onSubmit(): void {
+    this.submitted = true;
+    if (this.editForm.valid) {
+      const formData = this.editForm.value;
+      // Convert the start date back to the format required by the database
+      formData.start = new Date(formData.start).toISOString();
+      
+      this.tripDataService.updateTrip(formData)
+        .subscribe({
+          next: (value: any) => {
+            console.log(value);
+            this.router.navigate(['']);
+          },
+          error: (error: any) => {
+            console.log('Error: ' + error);
+          }
+        });
+    }
+  }
+
+  // easy access to form fields
+  get f() {
+    return this.editForm.controls;
+  }
+}
